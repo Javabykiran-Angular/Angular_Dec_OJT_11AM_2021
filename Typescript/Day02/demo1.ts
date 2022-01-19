00b7bec5d58ac4ae8fa8553fbea67741d43e902d
Javabykiran-Angular <71135924+Javabykiran-Angular@users.noreply.github.com>
2022-01-19 12:25:52 +0530

    Day02

---
+++ Typescript/Day02/demo1.ts
@@ -0,0 +1,10 @@
+
+console.log("Welcome to Angular Module")
+
+var a1;
+var a2,a3;
+
+//data type
+// number(int,float,double,long,short),string(""/''),boolean(true/false)
+//any (number,string,boolean), void(neutral => does not point anthing)
+//misec. null(object) & undefined(variable)
