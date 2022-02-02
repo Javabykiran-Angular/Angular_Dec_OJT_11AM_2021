5f70a4fcee96f133545fe7c59e7726c2d9804a12
Javabykiran-Angular <71135924+Javabykiran-Angular@users.noreply.github.com>
2022-02-02 13:32:34 +0530

    Day02

---
+++ Angular/Day02/myproduct/myproduct.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-myproduct',
+  templateUrl: './myproduct.component.html',
+  styleUrls: ['./myproduct.component.css']
+})
+export class MyproductComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ Angular/Day02/stringinterpolation/stringinterpolation.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-stringinterpolation',
+  templateUrl: './stringinterpolation.component.html',
+  styleUrls: ['./stringinterpolation.component.css']
+})
+export class StringinterpolationComponent implements OnInit {
+  name:string='Sumit Raokhande';
+  num1:number=2;
+  imgUrl="../../assets/bg.jpeg";
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
