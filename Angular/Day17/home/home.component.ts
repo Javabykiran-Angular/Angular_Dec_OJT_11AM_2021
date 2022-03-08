1b51a3d5d384334a160e5c8a8ef1f729d2a3e08a
Javabykiran-Angular <71135924+Javabykiran-Angular@users.noreply.github.com>
2022-03-08 12:18:30 +0530

    Day17

---
+++ Angular/Day17/home/home.component.ts
@@ -0,0 +1,45 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent implements OnInit {
+
+  myid:number=9;
+  myusername:string='Sumit Raokhande';
+  myjson={
+    id:8,
+    fname:'sumit',
+    lname:'Raokhande'
+  }
+  constructor(private router:Router) { }
+
+  ngOnInit() {
+  }
+
+  onPage1(){
+
+    this.router.navigate(['/page1']);
+  }
+  onPage2(){
+    this.myid=8
+    this.router.navigate(['/page2',this.myid])
+  }
+  onPage3(){
+    this.myid=8;
+    this.myusername='Spruha Raokhande';
+    this.router.navigate(['/page3',this.myid,this.myusername])
+  }
+  onFollower(){
+    this.router.navigate(['/Follower']);
+  }
+  onQueryparam(){
+    this.router.navigate(['/QueryParamter'],{
+      queryParams:this.myjson
+    })
+  }
+
+}
