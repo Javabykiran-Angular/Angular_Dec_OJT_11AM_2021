6eaa258636f8904623c98641e7c2c380ac936c03
Javabykiran-Angular <71135924+Javabykiran-Angular@users.noreply.github.com>
2022-03-14 12:32:40 +0530

    Day21

---
+++ Angular/Day21/demotdf/demotdf.component.ts
@@ -0,0 +1,39 @@
+import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
+@Component({
+  selector: 'app-demotdf',
+  templateUrl: './demotdf.component.html',
+  styleUrls: ['./demotdf.component.css']
+})
+export class DemotdfComponent implements OnInit {
+
+
+
+  country:any[]=[
+    {cid:1,name:'India'},
+    {cid:2,name:'US'},
+    {cid:3,name:'Japan'}
+  ];
+
+  EMAIL_PATTERN = "^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@"
+  + "[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$";
+
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  onSubmit(f:NgForm){
+
+    console.log("Email id is "+f.value.email);
+
+    let obj={
+      email:f.value.email,
+      username:f.value.myusername
+    }
+
+  }
+
+}
