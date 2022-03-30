a48ebd9ed5c423f7d21a35f37dd35c1455a65429
Javabykiran-Angular <71135924+Javabykiran-Angular@users.noreply.github.com>
2022-03-30 17:11:36 +0530

    Day31

---
+++ Angular/Day31/http.service.ts
@@ -0,0 +1,49 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HttpService {
+
+  private baseUrl:string='http://localhost:8080/api/';
+
+  constructor(private http:HttpClient) { }
+
+
+  loginCheck(obj){
+    
+   return (this.http.post(`${this.baseUrl}logincheck`,obj));
+  }
+
+  getEmployee(){
+     return (this.http.get(`${this.baseUrl}getallemployee`))
+  }
+
+  getEmployeeById(id){
+    return (this.http.get(`${this.baseUrl}getemployeebyid/${id}`));
+  }
+
+
+  getAllCountry(){
+    return  (this.http.get(`${this.baseUrl}getallcountry`));
+  }
+
+  addEmployee(obj){
+
+      return (this.http.post(`${this.baseUrl}addemployee`,obj,{
+        responseType:'text'
+      }))
+  }
+
+  updateEMP(obj){
+   return (this.http.put(`${this.baseUrl}updateemployee`,obj,{responseType:'text'}));
+  }
+
+  DeleteEMP(id){
+   return (this.http.delete(`${this.baseUrl}deleteemployee/${id}`,{responseType:'text'}));
+  }
+
+  
+
+}
